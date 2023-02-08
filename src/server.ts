b0dd5498508f2c3a19c8789c6eea80abc94c4428
Julianne Adams <julianne@julianneadams.info>
2023-02-08 11:12:39 -0500

    Add: Pino dependency
    This commit adds Pino as a dependency so we can do logging. It outputs
    to both standard output and a file based on the settings passed through
    environment variables.
    
    Also added is the gracefulShutdown() function. This will run whenever
    the program terminates and is meant to shutdown/disconnect all services
    cleanly before appliction termination.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import logger from "@utils/logger";
+
+logger.info("Hello World!");
+
+const gracefulShutdown = (cause: string) => {
+  logger.info({ cause }, "Closing HTTP server due to %s.");
+};
+
+process.on("uncaughtException", (err) => {
+  logger.error(err);
+});
+
+process.on("unhandledRejection", (err) => {
+  logger.error(err);
+});
+
+process.on("SIGTERM", () => gracefulShutdown("app termination"));
+process.on("SIGUSR2", () => gracefulShutdown("ts-node restart"));
+process.on("SIGINT", () => gracefulShutdown("app termination"));
