cbf31b5847c7459af717a38b5c7348165724ab76
Julianne Adams <julianne@julianneadams.info>
2023-06-14 12:02:55 -0400

    Add: user entity

---
+++ src/components/users/domain/user.ts
@@ -0,0 +1,79 @@
+import { Entity } from "@domain/entity";
+import { Name } from "@components/users/domain/name";
+import { HashedPassword } from "@components/users/domain/hashedPassword";
+import { EmailAddress } from "@components/users/domain/emailAddress";
+import { UniqueEntityId } from "@domain/uniqueEntityId";
+import { Result } from "@utils/result";
+import { Guard, IGuardResult } from "@utils/guard";
+
+interface IUserProps {
+  firstName: Name;
+  lastName: Name;
+  emailAddress: EmailAddress;
+  hashedPassword: HashedPassword;
+  createdAt: Date;
+  updatedAt: Date | undefined;
+  deletedAt: Date | undefined;
+}
+
+class User extends Entity<IUserProps> {
+  get uuid(): UniqueEntityId {
+    return this.id;
+  }
+
+  get firstName(): Name {
+    return this.props.firstName;
+  }
+
+  get lastName(): Name {
+    return this.props.lastName;
+  }
+
+  get emailAddress(): EmailAddress {
+    return this.props.emailAddress;
+  }
+
+  get hashedPassword(): HashedPassword {
+    return this.props.hashedPassword;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date | undefined {
+    return this.props.updatedAt;
+  }
+
+  get deletedAt(): Date | undefined {
+    return this.props.deletedAt;
+  }
+
+  /* eslint-disable-next-line no-useless-constructor */
+  private constructor(props: IUserProps, id?: UniqueEntityId) {
+    super(props, id);
+  }
+
+  public static createUser(
+    props: IUserProps,
+    id?: UniqueEntityId,
+  ): Result<User> {
+    const userPropsCheck: IGuardResult = Guard.againstNullOrUndefinedBulk([
+      { argumentName: "firstName", argument: props.firstName },
+      { argumentName: "lastName", argument: props.lastName },
+      { argumentName: "emailAddress", argument: props.emailAddress },
+      { argumentName: "hashedPassword", argument: props.hashedPassword },
+      { argumentName: "createdAt", argument: props.createdAt },
+    ]);
+
+    if (!userPropsCheck.succeeded) {
+      return Result.fail<User>(userPropsCheck.message as string);
+    }
+
+    return Result.ok<User>(new User(props, id));
+  }
+}
+
+type UserCollection = User[];
+
+export { User, UserCollection };
--- src/domain/slug.ts
+++ src/domain/slug.ts
@@ -7,15 +7,15 @@ interface SlugProps {
 }
 
 class Slug extends ValueObject<SlugProps> {
-  private static targetLength = 22;
-
-  get value(): string {
-    return this.props.value;
-  }
+  private static targetLength = 16;
 
   /* eslint-disable-next-line no-useless-constructor */
-  private constructor(props: SlugProps) {
-    super(props);
+  constructor(props?: SlugProps, uuid?: string) {
+    if (!props || !props.value) {
+      super({ value: Slug.generate(uuid!) });
+    } else {
+      super(props);
+    }
   }
 
   public static create(slug: string): Result<Slug> {
@@ -32,6 +32,17 @@ class Slug extends ValueObject<SlugProps> {
     }
 
     return Result.ok<Slug>(new Slug({ value: slug }));
+  }
+
+  private static generate(uuid: string): string {
+    const encodedUUID = Buffer.from(uuid).toString("base64");
+    const slug = encodedUUID
+      .replace("=", "")
+      .replace("+", "-")
+      .replace("/", "-")
+      .substring(0, 16);
+
+    return slug;
   }
 }
 
